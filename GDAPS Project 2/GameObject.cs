ce9ba2ac15826a3fa807779ee48e47967cd5c35e
lutem <sxl4023@g.rit.edu>
2015-10-16 15:47:08 -0400

    new classes

--- GDAPS Project 2/GameObject.cs
+++ GDAPS Project 2/GameObject.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace GDAPS_Project_2
 {
-    class GameObject
+    abstract class GameObject
     {
+        Vector2 objImage;
+        Rectangle objRect;
+        Texture objTexture;
+
+        public abstract bool isColliding();
+        public void spriteDraw(SpriteBatch s)
+        {
+            this.spriteDraw(s);
+        }
     }
 }
