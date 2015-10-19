6000885793a3ad61bd61d775f86ae8de8c47d64b
lutem <sxl4023@g.rit.edu>
2015-10-19 15:23:53 -0400

    started on level constructor, added list of levels to world

--- GDAPS Project 2/Level.cs
+++ GDAPS Project 2/Level.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,9 +10,43 @@ namespace GDAPS_Project_2
     class Level
     {
         int levelTime;
-        public Level(int t)
+        StreamReader s;
+        public Level(int t, string levelData)
         {
             levelTime = t;
+            try
+            {
+                char delim = ',';
+                string identifier;
+                Point p;
+                s = new StreamReader(levelData);
+                string line;
+                string[] splitLine;
+                while((line = s.ReadLine()) != null)
+                {
+                    try
+                    {
+                        splitLine = line.Split(delim);
+                        identifier = splitLine[0];
+                        p = new Point(int.Parse(splitLine[1]), int.Parse(splitLine[2])); // TODO check up and normalize string format for levels
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if(s != null)
+                {
+                    s = null;
+                }
+            }
         }
     }
 }
--- GDAPS Project 2/World.cs
+++ GDAPS Project 2/World.cs
@@ -8,5 +8,13 @@ namespace GDAPS_Project_2
     class World
     {
         List<Level> levels = new List<Level>();
+
+        public World(int world)
+        {
+            switch(world) { // TODO set levels depending on which world is selected using cases
+                default:
+                    break;
+            }
+        }
     }
 }
