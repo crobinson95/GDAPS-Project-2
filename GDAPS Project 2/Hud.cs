1ecb244cb34c516e1fe293b021be5c81b0c8835e
lutem <sxl4023@g.rit.edu>
2015-10-26 16:04:02 -0400

    added HUD class

---
+++ GDAPS Project 2/Hud.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDAPS_Project_2
+{
+    class Hud : GameObject
+    {
+        Player playerLoc;
+        SpriteBatch spriteBatch;
+        Level levelRelevantInfo;
+
+        public Hud(int x, int y, int w, int h, SpriteBatch s, Player p, Level l) : base(x, y, w, h)
+        {
+            spriteBatch = s;
+            playerLoc = p;
+            levelRelevantInfo = l;
+            // TODO: set HUD starting location, use the sprite batch to write/draw relevant information
+            // use player to get player location and draw HUD on the top/bottom depending on location
+            // add a string to each level that provides relevant information that needs to be displayed
+        }
+
+    }
+}
